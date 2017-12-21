419c8c422ad329ce54b6bea135857007da5b4146
maroro0220 <maro1339@gamil.com>
2017-12-21 02:47:10 +0000

    user join&login

---
+++ 20171221/post/app/controllers/post_controller.rb
@@ -0,0 +1,51 @@
+class PostController < ApplicationController
+  def index
+    @post=PostList.all
+  end
+
+  def edit
+    @post=PostList.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    title=params[:title]
+    name=params[:name]
+    text=params[:text]
+    PostList.create(title: title, name: name, text: text)
+    redirect_to '/'
+  end
+
+  def update
+    post=PostList.find(params[:id])
+    post.update(title: params[:title],name: params[:name], text: params[:text])
+    redirect_to "/post/show/#{params[:id]}"
+  end
+
+  def destroy
+    post=PostList.find(params[:id])
+    post.destroy
+    redirect_to '/'
+  end
+
+  def show
+    @p=PostList.find(params[:id])
+    @c=Comment.all
+  end
+  def add_comment
+    Comment.create(name: params[:name], comm: params[:text], post_list_id: params[:id] )
+    redirect_to :back
+  end
+  def comdel
+    comment = Comment.find(params[:id])
+    post = comment.post_list_id
+    comment.destroy
+    redirect_to "/post/show/#{post}"
+  end
+  def comedit
+
+  end
+
+end
---
+++ 20171221/post/app/controllers/user_controller.rb
@@ -0,0 +1,39 @@
+class UserController < ApplicationController
+  def new
+    #사용자에게 form 창을 보여주는 곳
+
+  end
+  def create
+    #정보를 받아서 데이터베이스에서 만드는 c
+    User.create(name: params[:name], email: params[:email], password: params[:password])
+    @user=User.last
+  end
+  def index
+    @users=User.all
+  end
+  def LOGIN
+    #로그인을 위한 form
+
+  end
+
+  def login_process
+    #1. mail주소가 우리 db없으면,(가입한 적이 없으면)
+    #1-1. 없는 아이디 -> 회원가입
+    user=User.find_by(email: params[:email])
+    @msg=""
+    if user.nil?
+      @msg="noononomember"
+    #2. mail주소가 db에 있으면,
+    #2-1 비밀번호 확인. 맞음 로그인인
+    #2-2 틀리면, 비밀번호가 틀렸습니다.
+  else
+    if user.password == params[:password]
+        @msg="login Success"
+        session[:user_id]=user.id
+    else
+      @msg="password error"
+    end
+  end
+  end
+
+end
